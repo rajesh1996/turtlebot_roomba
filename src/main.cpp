d4e1ab3c43538ae6775cc229bed73e3bc42614e5
rajesh1996 <rns@umd.edu>
2020-11-23 19:19:20 -0500

    [Main] updating main with rosrate

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
-//
-// Created by rajesh on 11/21/20.
-//
+#include "roomba.h"
 
+int main(int argc,char* argv[]) {
+    ros::init(argc,argv,"turtlebot_roomba");
+    Roomba_bot roomba;
+    ros::Rate loop(7);
+    while (ros::ok()) {
+        roomba.walkRoomba();
+        ros::spinOnce();
+        loop.sleep();
+    }
+    return 0;
+}
