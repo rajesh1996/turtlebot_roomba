99c42680dff5e17170e40e25b7e407a06d838aa1
rajesh1996 <rns@umd.edu>
2020-11-23 19:21:05 -0500

    [roomba.cpp] Limiting lidar scan for obstacle checking

--- src/roomba.cpp
+++ src/roomba.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
-#include <roomba.cpp>
+#include "roomba.h"
 
 Roomba_bot::Roomba_bot() {
-    lin_vel = 0.9;
+
+    pub_vel = nh.advertise <geometry_msgs::Twist>
+            ("/cmd_vel", 1000);
+
+    sub = nh.subscribe<sensor_msgs::LaserScan>("/scan",1000, &Roomba_bot::laserCallback, this);
+    lin_vel = 0.2;
     ang_vel = 1.0;
     msg.linear.x=0.0;
     msg.linear.y=0.0;
@@ -10,6 +15,9 @@ Roomba_bot::Roomba_bot() {
     msg.angular.x=0.0;
     msg.angular.y=0.0;
     msg.angular.z=0.0;
+    pub_vel.publish(msg);
+    obstacle = false;
+
 }
 
 Roomba_bot::~Roomba_bot() {
@@ -19,23 +27,42 @@ Roomba_bot::~Roomba_bot() {
     msg.angular.x=0.0;
     msg.angular.y=0.0;
     msg.angular.z=0.0;
+    pub_vel.publish(msg);
 
 }
 
 void Roomba_bot::laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {
-    for(auto i=0;i<msg->ranges.size();i++)
-    {
-        if (msg->ranges[i]<0.9) {
-            obstacle = true;
-            return;
-        }
-
+    if (msg->ranges[0] > 0.8 && msg->ranges[15] > 0.8 && msg->ranges[345] > 0.8) {
+        setObstacle(false);
+    } else {
+        setObstacle(true);
     }
-    obstacle = false;
 }
 
 bool Roomba_bot::checkObstacle() {
     return obstacle;
 }
+
+bool Roomba_bot::setObstacle(bool obs) {
+    obstacle = obs;
+    return true;
+}
+
+void Roomba_bot::walkRoomba() {
+
+        if (checkObstacle()) {
+            ROS_INFO_STREAM("Obstacle present- Turning");
+            msg.linear.x = 0.0;
+            msg.angular.z = ang_vel;
+        }
+        else
+        {
+            ROS_INFO_STREAM("Moving straight");
+            msg.angular.z = 0.0;
+            msg.linear.x = lin_vel;
+        }
+        pub_vel.publish(msg);
+    }
+
 
 
